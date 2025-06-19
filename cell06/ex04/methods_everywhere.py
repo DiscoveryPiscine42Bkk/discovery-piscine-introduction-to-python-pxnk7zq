91c9684df0d07ce1b5115a861c6569fd04531199
pxnk7zq <kanyaratpink2549@gmail.com>
2025-06-19 01:09:48 +0000

    ex04

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,16 @@
+import sys
+def shrink(text):
+    print(text[:8])
+def enlarge(text):
+    print(text + 'Z' * (8 - len(text)))
+args = sys.argv[1:]
+if len(args) < 1:
+    print("none")
+else:
+    for arg in args:
+        if len(arg) > 8:
+            shrink(arg)
+        elif len(arg) < 8:
+            enlarge(arg)
+        else:
+            print(arg)
