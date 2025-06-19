44be5fe701159570a7ab8ec6e0dd74083a5a8e25
pxnk7zq <kanyaratpink2549@gmail.com>
2025-06-19 01:24:07 +0000

    ex02

---
+++ cell07/ex02/help_your_professor.py
@@ -0,0 +1,20 @@
+def average(score_dict):
+    if not score_dict:
+        return 0
+    total = sum(score_dict.values())
+    count = len(score_dict)
+    return total / count
+class_3B = {
+    "marine": 18,
+    "jean": 15,
+    "coline": 8,
+    "luc": 9
+}
+class_3C = {
+    "quentin": 17,
+    "julie": 15,
+    "marc": 8,
+    "stephanie": 13
+}
+print(f"Average for class 3B: {average(class_3B)}.")
+print(f"Average for class 3C: {average(class_3C)}.")
